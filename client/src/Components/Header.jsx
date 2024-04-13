a08a02a3b1fdd5d314570f70f471af5a48ca7c62
Kartik malik <sanjaykartik2222@gmail.com>
2024-04-13 14:05:26 +0530

    Complete sign up page functionality

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -19,7 +19,7 @@ export default function Header() {
         <ul className="flex gap-4">
             <Link to="/"><li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">Home</li></Link>
             <Link to="/about"><li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">About</li></Link>
-            <Link to="/sign-in"><li className="inline text-slate-700 hover:underline cursor-pointer">Sign In</li></Link>
+            <Link to="/sign-up"><li className="inline text-slate-700 hover:underline cursor-pointer">Sign Up</li></Link>
             
         </ul>
       </div>
