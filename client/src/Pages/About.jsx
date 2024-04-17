ddf0d52ba0c65d3dfc9c7ecd45146b41f700c818
Kartik malik <sanjaykartik2222@gmail.com>
2024-04-17 13:32:28 +0530

    Completed about us page

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About Eazy Estate</h1>
+      <p className="mb04 text-slate-700">
+        Welcome to EazyEstate, your trusted destination for finding your perfect
+        place to call home. At EazyEstate, we are passionate about simplifying
+        the process of buying, selling, or renting properties, making it
+        effortless and enjoyable for our users.
+      </p>
+
+      <p className="mb04 text-slate-700">
+        Our mission is to empower individuals and families to discover their
+        dream homes by providing a comprehensive platform that offers a wide
+        range of properties to choose from. Whether you're searching for a cozy
+        apartment in the heart of the city, a spacious house in the suburbs, or
+        a luxurious villa by the beach, EazyEstate has got you covered.
+      </p>
+
+      <p className="mb04 text-slate-700">
+        With our user-friendly interface, advanced search filters, and
+        up-to-date listings, finding the ideal property has never been easier.
+        We prioritize transparency, reliability, and customer satisfaction,
+        ensuring that every interaction with EazyEstate is a positive and
+        fulfilling experience.
+      </p>
+
+      <p className="mb04 text-slate-700">
+        Backed by a team of experienced professionals and cutting-edge
+        technology, we strive to exceed your expectations and make your real
+        estate journey seamless and stress-free. Whether you're a first-time
+        buyer, seasoned investor, or homeowner looking to sell or rent your
+        property, EazyEstate is here to support you every step of the way.
+      </p>
+
+      <p>
+        Join the EazyEstate community today and embark on your journey towards
+        finding your perfect place to call home. Let's make your real estate
+        dreams a reality, the easy way, with EazyEstate.
+      </p>
     </div>
-  )
+  );
 }
